ee834336e443a08c1a6c2b2fb326a6e639510d8f
Ilya <iii@ya.ru>
2022-07-03 14:33:54 +0500

    task 10

--- Num10/Program.cs
+++ Num10/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+Console.WriteLine("Случайное трёхзначное число");
+int a = new Random().Next(100,1000);
+Console.WriteLine(a);
+
+int secondNum(int arg) {
+    int second = (arg%100)/10;
+    return second;
+}
+
+Console.WriteLine($"Вторая цифра числа -  {secondNum(a)}");
+
